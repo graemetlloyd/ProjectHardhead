6f2d1d7bb1fdb44d411063109ad3b701122ff2fe
Graeme T. Lloyd <graemetlloyd@gmail.com>
2019-11-06 16:51:09 +0000

    First addition of all data to repo.

---
+++ RScripts/CollateTrees.R
@@ -0,0 +1,89 @@
+# SCRIPT TO COLLATE TREES POST TNT INFERENCE
+
+# Load metatree library:
+library(metatree)
+
+# Read in TNT tree output:
+HypothesisOneSTRMPTs <- readLines("~/HypothesisOneSTRMPTs.nex", warn = FALSE)
+HypothesisTwoSTRMPTs <- readLines("~/HypothesisTwoSTRMPTs.nex", warn = FALSE)
+HypothesisThreeSTRMPTs <- readLines("~/HypothesisThreeSTRMPTs.nex", warn = FALSE)
+HypothesisFourSTRMPTs <- readLines("~/HypothesisFourSTRMPTs.nex", warn = FALSE)
+HypothesisFiveSTRMPTs <- readLines("~/HypothesisFiveSTRMPTs.nex", warn = FALSE)
+HypothesisSixSTRMPTs <- readLines("~/HypothesisSixSTRMPTs.nex", warn = FALSE)
+
+# Isolate and reformat trees as ape trees:
+HypothesisOneSTRMPTs <- ape::read.tree(text = gsub(" ", "", HypothesisOneSTRMPTs[which(unlist(lapply(strsplit(HypothesisOneSTRMPTs, ""), function(x) x[1] == "(")))]))
+HypothesisTwoSTRMPTs <- ape::read.tree(text = gsub(" ", "", HypothesisTwoSTRMPTs[which(unlist(lapply(strsplit(HypothesisTwoSTRMPTs, ""), function(x) x[1] == "(")))]))
+HypothesisThreeSTRMPTs <- ape::read.tree(text = gsub(" ", "", HypothesisThreeSTRMPTs[which(unlist(lapply(strsplit(HypothesisThreeSTRMPTs, ""), function(x) x[1] == "(")))]))
+HypothesisFourSTRMPTs <- ape::read.tree(text = gsub(" ", "", HypothesisFourSTRMPTs[which(unlist(lapply(strsplit(HypothesisFourSTRMPTs, ""), function(x) x[1] == "(")))]))
+HypothesisFiveSTRMPTs <- ape::read.tree(text = gsub(" ", "", HypothesisFiveSTRMPTs[which(unlist(lapply(strsplit(HypothesisFiveSTRMPTs, ""), function(x) x[1] == "(")))]))
+HypothesisSixSTRMPTs <- ape::read.tree(text = gsub(" ", "", HypothesisSixSTRMPTs[which(unlist(lapply(strsplit(HypothesisSixSTRMPTs, ""), function(x) x[1] == "(")))]))
+
+# Write STR trees to file:
+write.tree(HypothesisOneSTRMPTs, "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisOne/Trees/STRTrees.tre")
+write.tree(HypothesisTwoSTRMPTs, "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisTwo/Trees/STRTrees.tre")
+write.tree(HypothesisThreeSTRMPTs, "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisThree/Trees/STRTrees.tre")
+write.tree(HypothesisFourSTRMPTs, "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFour/Trees/STRTrees.tre")
+write.tree(HypothesisFiveSTRMPTs, "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFive/Trees/STRTrees.tre")
+write.tree(HypothesisSixSTRMPTs, "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisSix/Trees/STRTrees.tre")
+
+# Read in STR tables:
+HypothesisOneSTRTable <- read.table("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisOne/MetatreeFiles/STR.txt", header = TRUE, stringsAsFactors = FALSE)
+HypothesisTwoSTRTable <- read.table("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisTwo/MetatreeFiles/STR.txt", header = TRUE, stringsAsFactors = FALSE)
+HypothesisThreeSTRTable <- read.table("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisThree/MetatreeFiles/STR.txt", header = TRUE, stringsAsFactors = FALSE)
+HypothesisFourSTRTable <- read.table("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFour/MetatreeFiles/STR.txt", header = TRUE, stringsAsFactors = FALSE)
+HypothesisFiveSTRTable <- read.table("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFive/MetatreeFiles/STR.txt", header = TRUE, stringsAsFactors = FALSE)
+HypothesisSixSTRTable <- read.table("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisSix/MetatreeFiles/STR.txt", header = TRUE, stringsAsFactors = FALSE)
+
+# Safely reinsert the missing taxa to generate full trees:
+Claddis::SafeTaxonomicReinsertion(treefile.in = "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisOne/Trees/STRTrees.tre", treefile.out = "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisOne/Trees/FullTrees.tre", str.list = HypothesisOneSTRTable, multi.placements = "random")
+Claddis::SafeTaxonomicReinsertion(treefile.in = "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisTwo/Trees/STRTrees.tre", treefile.out = "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisTwo/Trees/FullTrees.tre", str.list = HypothesisTwoSTRTable, multi.placements = "random")
+Claddis::SafeTaxonomicReinsertion(treefile.in = "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisThree/Trees/STRTrees.tre", treefile.out = "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisThree/Trees/FullTrees.tre", str.list = HypothesisThreeSTRTable, multi.placements = "random")
+Claddis::SafeTaxonomicReinsertion(treefile.in = "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFour/Trees/STRTrees.tre", treefile.out = "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFour/Trees/FullTrees.tre", str.list = HypothesisFourSTRTable, multi.placements = "random")
+Claddis::SafeTaxonomicReinsertion(treefile.in = "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFive/Trees/STRTrees.tre", treefile.out = "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFive/Trees/FullTrees.tre", str.list = HypothesisFiveSTRTable, multi.placements = "random")
+Claddis::SafeTaxonomicReinsertion(treefile.in = "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisSix/Trees/STRTrees.tre", treefile.out = "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisSix/Trees/FullTrees.tre", str.list = HypothesisSixSTRTable, multi.placements = "random")
+
+# Read full trees in:
+HypothesisOneFullMPTs <- read.tree("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisOne/Trees/FullTrees.tre")
+HypothesisTwoFullMPTs <- read.tree("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisTwo/Trees/FullTrees.tre")
+HypothesisThreeFullMPTs <- read.tree("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisThree/Trees/FullTrees.tre")
+HypothesisFourFullMPTs <- read.tree("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFour/Trees/FullTrees.tre")
+HypothesisFiveFullMPTs <- read.tree("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFive/Trees/FullTrees.tre")
+HypothesisSixFullMPTs <- read.tree("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisSix/Trees/FullTrees.tre")
+
+# Ladderize each tree and drop allzero outgroup:
+HypothesisOneFullMPTs <- lapply(HypothesisOneFullMPTs, function(x) ape::drop.tip(ape::ladderize(x, right = FALSE), "allzero"))
+HypothesisTwoFullMPTs <- lapply(HypothesisTwoFullMPTs, function(x) ape::drop.tip(ape::ladderize(x, right = FALSE), "allzero"))
+HypothesisThreeFullMPTs <- lapply(HypothesisThreeFullMPTs, function(x) ape::drop.tip(ape::ladderize(x, right = FALSE), "allzero"))
+HypothesisFourFullMPTs <- lapply(HypothesisFourFullMPTs, function(x) ape::drop.tip(ape::ladderize(x, right = FALSE), "allzero"))
+HypothesisFiveFullMPTs <- lapply(HypothesisFiveFullMPTs, function(x) ape::drop.tip(ape::ladderize(x, right = FALSE), "allzero"))
+HypothesisSixFullMPTs <- lapply(HypothesisSixFullMPTs, function(x) ape::drop.tip(ape::ladderize(x, right = FALSE), "allzero"))
+
+# Reset class to multiPhylo:
+class(HypothesisOneFullMPTs) <- class(HypothesisTwoFullMPTs) <- class(HypothesisThreeFullMPTs) <- class(HypothesisFourFullMPTs) <- class(HypothesisFiveFullMPTs) <- class(HypothesisSixFullMPTs) <- "multiPhylo"
+
+# Write out reformatted trees:
+write.tree(HypothesisOneFullMPTs, "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisOne/Trees/FullTrees.tre")
+write.tree(HypothesisTwoFullMPTs, "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisTwo/Trees/FullTrees.tre")
+write.tree(HypothesisThreeFullMPTs, "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisThree/Trees/FullTrees.tre")
+write.tree(HypothesisFourFullMPTs, "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFour/Trees/FullTrees.tre")
+write.tree(HypothesisFiveFullMPTs, "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFive/Trees/FullTrees.tre")
+write.tree(HypothesisSixFullMPTs, "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisSix/Trees/FullTrees.tre")
+
+# Make consensus trees:
+HypothesisOneFullSC <- consensus(HypothesisOneFullMPTs)
+HypothesisTwoFullSC <- consensus(HypothesisTwoFullMPTs)
+HypothesisThreeFullSC <- consensus(HypothesisThreeFullMPTs)
+HypothesisFourFullSC <- consensus(HypothesisFourFullMPTs)
+HypothesisFiveFullSC <- consensus(HypothesisFiveFullMPTs)
+HypothesisSixFullSC <- consensus(HypothesisSixFullMPTs)
+
+# Build PDF of consensus trees for visualisation:
+pdf("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/PDFTrees/ConstraintConsensusTrees.pdf", width = 10, height = 25)
+plot(HypothesisOneFullSC, cex = 0.3, main = "1. Monophyletic Lissamphibia inside Temnospondyli")
+plot(HypothesisTwoFullSC, cex = 0.3, main = "2. Monophyletic Lissamphibia inside Lepospondyli")
+plot(HypothesisThreeFullSC, cex = 0.3, main = "3. Diphyletic Lissamphibia (Batrachia inside Temnospondyli and Gymnophiona inside Lepospondyli)")
+plot(HypothesisFourFullSC, cex = 0.3, main = "4. Diphyletic Lissamphibia inside Temnospondyli (Batrachia and Gymnophiona as separate clades)")
+plot(HypothesisFiveFullSC, cex = 0.3, main = "5. Triphyletic Lissamphibia (Anuri and Caudata inside Temnospondyli,\nGymniophona inside Lepospondyli and Crown Lissamphibia monophyletic)")
+plot(HypothesisSixFullSC, cex = 0.3, main = "6. Triphyletic Lissamphibia (Anuri and Caudata inside Temnospondyli,\nGymniophona inside Lepospondyli and Crown Lissamphibia paraphyletic to Amniota)")
+dev.off()
---
+++ RScripts/StratCongruenceTests.R
@@ -0,0 +1,64 @@
+# SCRIPT TO PERFORM STRAT CONGRUENCE TESTS ON EACH TREE
+
+# Load metatree library:
+library(metatree)
+library(paleotree)
+library(strap)
+
+# Read full trees in:
+HypothesisOneFullMPTs <- read.tree("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisOne/Trees/FullTrees.tre")
+HypothesisTwoFullMPTs <- read.tree("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisTwo/Trees/FullTrees.tre")
+HypothesisThreeFullMPTs <- read.tree("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisThree/Trees/FullTrees.tre")
+HypothesisFourFullMPTs <- read.tree("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFour/Trees/FullTrees.tre")
+HypothesisFiveFullMPTs <- read.tree("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFive/Trees/FullTrees.tre")
+HypothesisSixFullMPTs <- read.tree("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisSix/Trees/FullTrees.tre")
+
+# Read in first appearance ages:
+FirstAppearanceAges <- read.table("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/AgeData/FirstAppearanceAgeData.txt", header = TRUE, stringsAsFactors = FALSE, row.names = 1)
+
+# Add column names to first appearance ages:
+colnames(FirstAppearanceAges) <- c("FAD", "LAD")
+
+# Pool all trees together:
+Trees <- c(HypothesisOneFullMPTs, HypothesisTwoFullMPTs, HypothesisThreeFullMPTs, HypothesisFourFullMPTs, HypothesisFiveFullMPTs, HypothesisSixFullMPTs)
+
+# Perform strat congruence tests on pooled trees:
+x <- StratPhyloCongruence(trees = Trees, ages = FirstAppearanceAges, hard = FALSE, randomly.sample.ages = TRUE, fix.topology = TRUE, fix.outgroup = TRUE, outgroup.taxon = "Acanthostega_gunnari")
+
+# Write results tables to file:
+write.table(x$input.tree.results, "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/StratCongruenceResults/InputTreeResults.txt")
+write.table(x$rand.permutations, "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/StratCongruenceResults/RandomTreeResults.txt")
+
+# Write time-scaled trees to file:
+ape::write.tree(x$input.trees[1:1000], "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisOne/Trees/FullTreesTS.tre")
+ape::write.tree(x$input.trees[1001:2000], "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisTwo/Trees/FullTreesTS.tre")
+ape::write.tree(x$input.trees[2001:3000], "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisThree/Trees/FullTreesTS.tre")
+ape::write.tree(x$input.trees[3001:4000], "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFour/Trees/FullTreesTS.tre")
+ape::write.tree(x$input.trees[4001:5000], "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisFive/Trees/FullTreesTS.tre")
+ape::write.tree(x$input.trees[5001:6000], "~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/HypothesisSix/Trees/FullTreesTS.tre")
+
+# Read back in data (so can run these lines again without re-doing strat congruence tests):
+x <- list(input.tree.results = read.table("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/StratCongruenceResults/InputTreeResults.txt", header = TRUE), rand.permutations = read.table("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/StratCongruenceResults/RandomTreeResults.txt", header = TRUE))
+
+# Build boxplot data lists:
+BoxPlotMIG <- list(HypothesisOne = unname(x$input.tree.results[1:1000, "MIG"]), HypothesisTwo = unname(x$input.tree.results[1001:2000, "MIG"]), HypothesisThree = unname(x$input.tree.results[2001:3000, "MIG"]), HypothesisFour = unname(x$input.tree.results[3001:4000, "MIG"]), HypothesisFive = unname(x$input.tree.results[4001:5000, "MIG"]), HypothesisSix = unname(x$input.tree.results[5001:6000, "MIG"]))
+BoxPlotSCI <- list(HypothesisOne = unname(x$input.tree.results[1:1000, "SCI"]), HypothesisTwo = unname(x$input.tree.results[1001:2000, "SCI"]), HypothesisThree = unname(x$input.tree.results[2001:3000, "SCI"]), HypothesisFour = unname(x$input.tree.results[3001:4000, "SCI"]), HypothesisFive = unname(x$input.tree.results[4001:5000, "SCI"]), HypothesisSix = unname(x$input.tree.results[5001:6000, "SCI"]))
+BoxPlotRCI <- list(HypothesisOne = unname(x$input.tree.results[1:1000, "RCI"]), HypothesisTwo = unname(x$input.tree.results[1001:2000, "RCI"]), HypothesisThree = unname(x$input.tree.results[2001:3000, "RCI"]), HypothesisFour = unname(x$input.tree.results[3001:4000, "RCI"]), HypothesisFive = unname(x$input.tree.results[4001:5000, "RCI"]), HypothesisSix = unname(x$input.tree.results[5001:6000, "RCI"]))
+BoxPlotGER <- list(HypothesisOne = unname(x$input.tree.results[1:1000, "GER"]), HypothesisTwo = unname(x$input.tree.results[1001:2000, "GER"]), HypothesisThree = unname(x$input.tree.results[2001:3000, "GER"]), HypothesisFour = unname(x$input.tree.results[3001:4000, "GER"]), HypothesisFive = unname(x$input.tree.results[4001:5000, "GER"]), HypothesisSix = unname(x$input.tree.results[5001:6000, "GER"]))
+BoxPlotMSMStar <- list(HypothesisOne = unname(x$input.tree.results[1:1000, "MSM."]), HypothesisTwo = unname(x$input.tree.results[1001:2000, "MSM."]), HypothesisThree = unname(x$input.tree.results[2001:3000, "MSM."]), HypothesisFour = unname(x$input.tree.results[3001:4000, "MSM."]), HypothesisFive = unname(x$input.tree.results[4001:5000, "MSM."]), HypothesisSix = unname(x$input.tree.results[5001:6000, "MSM."]))
+BoxPlotGERStar <- list(HypothesisOne = unname(x$input.tree.results[1:1000, "GER."]), HypothesisTwo = unname(x$input.tree.results[1001:2000, "GER."]), HypothesisThree = unname(x$input.tree.results[2001:3000, "GER."]), HypothesisFour = unname(x$input.tree.results[3001:4000, "GER."]), HypothesisFive = unname(x$input.tree.results[4001:5000, "GER."]), HypothesisSix = unname(x$input.tree.results[5001:6000, "GER."]))
+BoxPlotGERt <- list(HypothesisOne = unname(x$input.tree.results[1:1000, "GERt"]), HypothesisTwo = unname(x$input.tree.results[1001:2000, "GERt"]), HypothesisThree = unname(x$input.tree.results[2001:3000, "GERt"]), HypothesisFour = unname(x$input.tree.results[3001:4000, "GERt"]), HypothesisFive = unname(x$input.tree.results[4001:5000, "GERt"]), HypothesisSix = unname(x$input.tree.results[5001:6000, "GERt"]))
+
+# Plot boxplots to PDF:
+pdf("~/Documents/Publications/in prep/Temnospondyl Supertree 2 - Dan/Metatree/BoxPlots/StratCongruenceMeasure.pdf", width = 12, height = 7, useDingbats = FALSE)
+boxplot(BoxPlotMIG, ylab = "Minimum Implied Gap", main = "Minimum Implied Gap", names = c(expression('H'[1]), expression('H'[2]), expression('H'[3]), expression('H'[4]), expression('H'[5]), expression('H'[6])), pch = 20, cex = 1.5, lwd = 1.5, cex.axis = 1.5, cex.lab = 1.5, cex.main = 1.5, axes = TRUE)
+#axis(1, lwd = 1.5, at = 1:6, labels = c(expression('H'[1]), expression('H'[2]), expression('H'[3]), expression('H'[4]), expression('H'[5]), expression('H'[6])), par(cex = 1.5))
+#axis(2, lwd = 1.5, at = c(7400, 7800, 8200, 8600), par(cex = 1.5))
+#box(lwd = 1.5)
+boxplot(BoxPlotSCI, ylab = "Stratigraphic Consistency Index", main = "Stratigraphic Consistency Index", names = c(expression('H'[1]), expression('H'[2]), expression('H'[3]), expression('H'[4]), expression('H'[5]), expression('H'[6])), pch = 20, cex = 1.5, lwd = 1.5, cex.axis = 1.5, cex.lab = 1.5, cex.main = 1.5, axes = TRUE)
+boxplot(BoxPlotRCI, ylab = "Relative Completeness Index", main = "Relative Completeness Index", names = c(expression('H'[1]), expression('H'[2]), expression('H'[3]), expression('H'[4]), expression('H'[5]), expression('H'[6])), pch = 20, cex = 1.5, lwd = 1.5, cex.axis = 1.5, cex.lab = 1.5, cex.main = 1.5, axes = TRUE)
+boxplot(BoxPlotGER, ylab = "Gap Excess Ratio", main = "Gap Excess Ratio", names = c(expression('H'[1]), expression('H'[2]), expression('H'[3]), expression('H'[4]), expression('H'[5]), expression('H'[6])), pch = 20, cex = 1.5, lwd = 1.5, cex.axis = 1.5, cex.lab = 1.5, cex.main = 1.5, axes = TRUE)
+boxplot(BoxPlotMSMStar, ylab = "Manhattan Stratigraphic Measure*", main = "Manhattan Stratigraphic Measure*", names = c(expression('H'[1]), expression('H'[2]), expression('H'[3]), expression('H'[4]), expression('H'[5]), expression('H'[6])), pch = 20, cex = 1.5, lwd = 1.5, cex.axis = 1.5, cex.lab = 1.5, cex.main = 1.5, axes = TRUE)
+boxplot(BoxPlotGERStar, ylab = "Gap Excess Ratio*", main = "Gap Excess Ratio*", names = c(expression('H'[1]), expression('H'[2]), expression('H'[3]), expression('H'[4]), expression('H'[5]), expression('H'[6])), pch = 20, cex = 1.5, lwd = 1.5, cex.axis = 1.5, cex.lab = 1.5, cex.main = 1.5, axes = TRUE)
+boxplot(BoxPlotGERt, ylab = "Gap Excess Ratiot", main = "Gap Excess Ratiot", names = c(expression('H'[1]), expression('H'[2]), expression('H'[3]), expression('H'[4]), expression('H'[5]), expression('H'[6])), pch = 20, cex = 1.5, lwd = 1.5, cex.axis = 1.5, cex.lab = 1.5, cex.main = 1.5, axes = TRUE)
+dev.off()
